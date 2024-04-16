aac47393187d700aa2d8cedb447a0b8cec51aa85
Jack Crawford <primes_unit.05@icloud.com>
2024-04-16 21:35:21 +1200

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from 'react'
 import AboutText from './components/aboutText/AboutText'
-import MainImage from './components/MainImage'
 import Contact from './components/Contact'
 import Toggle from './components/Toggle'
 
@@ -11,7 +10,7 @@ function Home() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white ${backgroundColor}`}
+      className={`flex min-h-screen flex-col items-center justify-center p-24 bg-black text-white ${backgroundColor}`}
     >
       <span className="absolute top-0 right-0 mt-4 mr-4">
         <Toggle setBackgroundColor={setBackgroundColor} />
@@ -19,7 +18,6 @@ function Home() {
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-sans text-7xl lg:flex font-extrabold ">
         <AboutText />
-        <MainImage />
       </div>
       <Contact />
     </main>
