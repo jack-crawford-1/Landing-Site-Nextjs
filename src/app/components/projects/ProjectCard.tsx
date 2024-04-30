556ec087e6fd0b1c7a3222ecd42baff86b1a8b3d
Jack Crawford <primes_unit.05@icloud.com>
2024-04-30 20:20:49 +1200

    styled projects page for diff screen sizes

--- src/app/components/projects/ProjectCard.tsx
+++ src/app/components/projects/ProjectCard.tsx
@@ -21,8 +21,8 @@ function ProjectCard({
   const techStack = technologies || []
   return (
     <div className="container mx-auto p-10 pt-2 bg-gray-800 min-w-3/4 md:w-2/5 md:rounded-lg md:border-4 border-2 border-l-0 border-r-0 md:m-5">
-      <div className="flex flex-row justify-between items-center  w-fit">
-        <div className="m-0 mr-2">
+      <div className="flex xl:flex-row sm:flex-col flex-col justify-between items-center w-fit">
+        <div className="m-0">
           <h2 className="text-2xl text-white m-2 hover:scale-110 hover:duration-500 ease-in-out">
             <a
               target="_blank"
@@ -35,7 +35,7 @@ function ProjectCard({
               </button>
             </a>
           </h2>
-          <div className="hover:scale-110 transform hover:duration-200 ease-in-out mb-5 rounded">
+          <div className="md:hover:scale-110 transform md:hover:duration-200 ease-in-out md:mb-5 mb-2 rounded">
             <Image
               src={imageSrc}
               alt={altText}
@@ -45,15 +45,15 @@ function ProjectCard({
             />
           </div>
         </div>
-        <div className="flex-1">
-          <p className="text-sm text-white m-2 -mr-5">{description}</p>
+        <div className="flex-1 m-2">
+          <p className="text-sm text-white m-2 ">{description}</p>
         </div>
       </div>
       <ul className="text-sm flex flex-row text-green-500 flex-wrap">
         {techStack.map((tech, index) => (
           <li
             key={index}
-            className="border-2 m-0.5 rounded hover:text-white p-3 max-h-10 content-center justify-center hover:scale-110 transform hover:duration-200 ease-in-out whitespace-nowrap"
+            className="border-2 m-1 rounded-xl md:hover:text-white p-3 max-h-10 content-center justify-center md:hover:scale-110 transform hover:duration-200 ease-in-out whitespace-nowrap"
           >
             {tech}
           </li>
@@ -61,7 +61,7 @@ function ProjectCard({
         <a
           href={githubLink}
           target="_blank"
-          className=" m-1 hover:text-white p-0 content-center justify-center hover:scale-110 transform hover:duration-700 ease-in-out w-max min-w-max mr-5"
+          className=" m-1 hover:text-white p-0 content-center justify-center hover:scale-150 transform hover:duration-700 ease-in-out w-max min-w-max "
         >
           <Image src="/github-logo.png" alt="github" width={30} height={30} />
         </a>
