91e6fbcbc47ca727cf4235821c111f6d08f97460
Jack Crawford <primes_unit.05@icloud.com>
2024-06-28 05:47:32 +1200

    text size

--- src/app/components/projects/HeaderArea.tsx
+++ src/app/components/projects/HeaderArea.tsx
@@ -2,7 +2,7 @@ import { ColourfulParagraph } from '../MainText/ColourfulParagraph'
 
 function HeaderArea() {
   return (
-    <div className="w-full text-white text-center items-center text-7xl font-extrabold p-10">
+    <div className="w-full text-white text-center items-center text-6xl md:text-7xl font-extrabold p-10">
       <ColourfulParagraph>PROJECTS</ColourfulParagraph>
     </div>
   )
