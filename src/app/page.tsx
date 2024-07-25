975c77aa0f64d2aa2903f9805978ad34138c2356
Jack Crawford <primes_unit.05@icloud.com>
2024-07-26 10:29:08 +1200

    hide scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import AboutSection from '../../pages/about'
 function Home() {
   return (
     <main
-      className={`flex flex-col items-center justify-center p-10 bg-slate-800 text-white `}
+      className={`flex flex-col items-center justify-center p-10 bg-slate-800 text-white overflow-x-hidden`}
     >
       <div className="max-w-5xl md:min-h-[600px] min-h-[200px] items-center justify-between text-7xl pl-10 md:pl-[150px] md:text-8xl lg:flex font-extrabold  ">
         <HeroText />
