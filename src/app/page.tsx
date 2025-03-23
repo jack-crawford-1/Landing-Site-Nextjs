e47cca767060d0885edbe26473cbb3edf7ef5f17
Jack Crawford <primes_unit.05@icloud.com>
2025-03-23 18:35:02 +1300

    mobile size fix 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import AboutSection from '../../pages/about';
 function Home() {
   return (
     <main
-      className={` overflow-x-hidden flex flex-col items-center justify-center md:p-10 p-3 bg-[#070706] text-white bg-fixed  bg-cover bg-center bg-[url(/bg-1.jpg)]`}
+      className={` overflow-x-hidden flex flex-col items-center justify-center md:p-10 p-3 bg-[#070706] text-white bg-fixed  bg-cover bg-center md:bg-[url(/bg-1.jpg)] bg-black`}
     >
       <div className="md:min-h-screen h-full flex flex-col justify-center ">
         <div className="max-w-5xl  min-h-[200px] mt-10 md:mt-0 items-start md:items-end justify-between md:pl-[150px] md:text-8xl lg:flex font-extrabold  ">
